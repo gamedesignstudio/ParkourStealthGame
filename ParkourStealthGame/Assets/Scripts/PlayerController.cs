a8f8762ce6c38cbbed15f134f56c4925aa0a234b
Jacob Greene <jgree178@uncc.edu>
2019-02-01 11:44:26 -0500

    animation stuff

--- ParkourStealthGame/Assets/Scripts/PlayerController.cs
+++ ParkourStealthGame/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Animation.Play("ybot@Walking");
     }
 
     // Update is called once per frame
