8f385a3c55b9b54cff58f3f39c2313807d5e3c32
ehatfie2 <ehatfie2@uncc.edu>
2019-02-22 18:03:22 -0500

    Started Animation Work
    Created a separate scene for animation work, created animation controller with idle and jogging "Transitions" for a y bot model. Animations are in place and occur based on a getkey. An animation script was created for the player model in order to keep the movement script separate. The playerMove script moves the entire player object, whereas the animTest script resides in the ybot assets that is a child of the player object!
    
    So far so good!
    
    Next objective is to implement sprinting transition and jumping, and maybe climbing.

---
+++ ParkourStealthGame/Assets/Eric's Stuff/Scripts/AnimTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour
+{
+
+    public Animator anim;
+    [SerializeField] private KeyCode runKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(runKey))
+            anim.SetBool("isJogging", true);
+        else
+            anim.SetBool("isJogging", false);
+
+    }
+}
