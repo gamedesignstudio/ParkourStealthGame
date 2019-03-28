c5a51c4295f8474136fa1dc049e1924d01a42a97
Jacob Greene <jgree178@uncc.edu>
2019-03-27 23:07:05 -0400

    Main Menu Implementation
    Implemented Main menu into game

---
+++ ParkourStealthGame/Assets/Jacob's Stuff/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField] private Object SceneToLoad;
+
+
+    public void PlayGame()
+    {
+        Debug.Log("Opening Scene: " + SceneToLoad);
+        SceneManager.LoadScene(SceneToLoad.name);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+}
