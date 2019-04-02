4822c3189795d9dbc758fe8bb5014ce6d675e651
bbrady6 <31266187+bbrady6@users.noreply.github.com>
2019-04-01 22:59:03 -0400

    Timer script made. debug log to check
    timer script was made. need to do UI

---
+++ ParkourStealthGame/Assets/Jacob's Stuff/Scripts/TimeController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    private int minutes;
+    private int seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minutes = 0;
+        seconds = 0;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CountSeconds();
+        Debug.Log("Timer  " + minutes + ":" + seconds);
+    }
+
+    //count seconds
+    private void CountSeconds()
+    {
+        //if 59 seconds, add 1 minute and reset seconds else continue counting
+        if(seconds == 59)
+        {
+            CountMinute();
+            seconds = 0;
+        }
+        else
+        {
+            seconds += 1;
+        }
+    }
+
+    //count minutes
+    private void CountMinute()
+    {
+        //if 1hr, end game, else continue counting
+        if(minutes > 60)
+        {
+            //end game
+            Debug.Log("Time over 1hr. end game");
+        }
+        else
+        {
+            minutes += 1;
+        }
+
+    }
+}
