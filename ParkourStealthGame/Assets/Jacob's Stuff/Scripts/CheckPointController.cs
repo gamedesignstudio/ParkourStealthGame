beb58c1c3c29b7ee2e42242d8a9a1dec168fe30e
bbrady6 <bbrady6@uncc.edu>
2019-04-08 11:26:31 -0400

    created checkpoints(not tested)
    need player in scene to test. please inset code from checkpoint controller into playerController

---
+++ ParkourStealthGame/Assets/Jacob's Stuff/Scripts/CheckPointController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointController : MonoBehaviour
+{
+    // put this in  playerContoller
+    //create variable for respawn location
+    //Vector3 respawnPoint;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        //check if player collides with a checkpoint
+        if(collider.tag.Equals("checkpoint"))
+        {
+            //get player's location when they pass the checkpoint and store it as the respawn location
+            //respawnPoint = this.Vector3;
+
+        }
+
+        if(collider.tag.Equals("finish"))
+        {
+            //stop timer
+            //store time on leader board
+            //reset respawn to first location
+            //reset timer
+            //go to end screen 
+        }
+    }
+}
