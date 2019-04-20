5d52663293011e8d68a058c43c7208ffc0d7b9ac
bbrady6 <31266187+bbrady6@users.noreply.github.com>
2019-04-20 00:40:45 -0400

    Strarted working on climbing

---
+++ ParkourStealthGame/Assets/Jacob's Stuff/Scripts/WallManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    bool isClimbing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isClimbing = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isClimbing)
+        {
+            player.Translate(Vector3.up * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if (Input.GetKeyDown("e"))
+            {
+                Debug.Log("Player is climbing vent");
+                isClimbing = true; 
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isClimbing = false;
+        }
+    }
+}
