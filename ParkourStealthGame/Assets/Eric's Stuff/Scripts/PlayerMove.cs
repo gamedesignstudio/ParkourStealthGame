a2021fe699279b88a7c4d7f01541898925c021fe
bbrady6 <31266187+bbrady6@users.noreply.github.com>
2019-04-26 13:38:13 -0400

    Basic movement is done.
    need to work on climbing and the jump. everything else seems to be working well

--- ParkourStealthGame/Assets/Eric's Stuff/Scripts/PlayerMove.cs
+++ ParkourStealthGame/Assets/Eric's Stuff/Scripts/PlayerMove.cs
@@ -4,127 +4,173 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    //controls the speed in which you move
+    private float speed;
+    [SerializeField] private float normalSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float crouchSpeed;
+    [SerializeField] private float jumpHeight;
 
-    [SerializeField] private string horizontalInputName;
-    [SerializeField] private string verticalInputName;
+    //controls for moving
+    [SerializeField] private KeyCode forward;
+    [SerializeField] private KeyCode backwards;
+    [SerializeField] private KeyCode left;
+    [SerializeField] private KeyCode right;
+    [SerializeField] private KeyCode jump;
+    [SerializeField] private KeyCode sprint;
+    [SerializeField] private KeyCode climb;
+    [SerializeField] private KeyCode crouch;
+    [SerializeField] private KeyCode slide;
 
-    [SerializeField] private float walkSpeed, runSpeed;
-    [SerializeField] private float runBuildUp, runSlowDown;
-
-    [SerializeField] private KeyCode runKey;
-
-
-     private float movementSpeed;
-
-    [SerializeField] private float slopeForce;
-    [SerializeField] private float slopeForceRayLength;
-
-    private CharacterController charController;
-
-    [SerializeField] private AnimationCurve jumpFallOff;
-    [SerializeField] private float jumpMultiplier;
-    [SerializeField] private KeyCode jumpKey;
-
-
-
+    //references to player's components
+    private Transform playerTrans;
+    private Rigidbody playerRigi;
+    private GameObject playerObj;
+    
+    //checks to see state of player
     private bool isJumping;
+    private bool isClimbing;
+    private bool isSliding;
+
+    //variables used to store checkpoint locations
+    public Vector3 respawnLoc;
 
     private void Awake()
     {
-        charController = GetComponent<CharacterController>();
+        
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerTrans = GetComponent<Transform>();
+        playerRigi = GetComponent<Rigidbody>();
+
+        //set the speed to normal (jogging) speed
+        speed = normalSpeed;
+
+        playerRigi.useGravity = true;
+        //store player's current location as the respawn location
+        respawnLoc = playerTrans.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        ChangeSpeed();
+        //call the player move function
         PlayerMovement();
+
+    }
+
+    void FixedUpdate()
+    {
+        /*
+        if (Input.GetKey(forward))
+        {
+
+            //move rigidbody forward
+            this.transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+            // playerRigi.velocity = new Vector3(speed, 0, 0);
+        }*/
+    }
+
+    private void ChangeSpeed()
+    {
+        //if crouch set speed to crouchSpeed; if sprint set speet to runSpeed; else speed = normalSpeed
+        if (Input.GetKey(crouch))
+        {
+            speed = crouchSpeed;
+        }
+        else if (Input.GetKey(sprint))
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = normalSpeed;
+        }
+
 
     }
 
     private void PlayerMovement()
     {
-        float vertInput = Input.GetAxisRaw(verticalInputName);
-        float horizInput = Input.GetAxisRaw(horizontalInputName);
+        //float horizInput = Input.GetAxisRaw("Horizontal");
+        //float vertInput = Input.GetAxis("Vertical");
+        //transform.Translate(horizInput * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
+        if (Input.GetKey(forward))
+        {
+            //move rigidbody forward
+            this.transform.position = transform.position + transform.forward * speed * Time.deltaTime;
+            // playerRigi.velocity = new Vector3(speed, 0, 0);
+        }
+        if (Input.GetKey(backwards))
+        {
+            //move rigidbody forward
+            this.transform.position = transform.position + -transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(left))
+        {
+            //move rigidbody forward
+            this.transform.position = transform.position + -transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(right))
+        {
+            //move rigidbody forward
+            this.transform.position = transform.position + transform.right * speed * Time.deltaTime;
+        }
+        
 
-        Vector3 forwardMovement = transform.forward * vertInput;
-        Vector3 rightMovement = transform.right * horizInput;
+        //create a vector 3 that tells the object where you want to go
+        //Vector3 movement = new Vector3(horizInput * speed * Time.deltaTime, 0, vertInput * speed * Time.deltaTime);
 
-        charController.SimpleMove(Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f) * movementSpeed);
+        // playerRigi.MovePosition(transform.position + movement);
 
-        if ((vertInput != 0 || horizInput != 0) && OnSlope())
-            charController.Move(Vector3.down * charController.height / 2 * slopeForce * Time.deltaTime);
-
-        if (charController.collisionFlags == CollisionFlags.Sides)
-            isJumping = false;
-
-
-        SetMovementSpeed();
 
         JumpInput();
 
     }
 
-    private void SetMovementSpeed()
-    {
-        if (Input.GetKey(runKey))
-            movementSpeed = Mathf.Lerp(movementSpeed, runSpeed, Time.deltaTime * runBuildUp);
-        else
-            movementSpeed = Mathf.Lerp(movementSpeed, walkSpeed, Time.deltaTime * runSlowDown);
-    }
-
-
-
     private bool OnSlope()
     {
-        if (isJumping)
-            return false;
 
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, charController.height / 2 * slopeForceRayLength))
-            if (hit.normal != Vector3.up)
-                return true;
         return false;
-
     }
 
     private void JumpInput()
     {
-        if (Input.GetKeyDown(jumpKey) && !isJumping)
+        if (Input.GetKey(jump))
         {
             isJumping = true;
-            StartCoroutine(JumpEvent());
+            //move rigidbody forward
+            this.transform.position = transform.position + transform.up * jumpHeight * Time.deltaTime;
+            isJumping = false;
         }
-        
+
     }
 
     private IEnumerator JumpEvent()
     {
-        float timeInAir = 0.0f;
-        charController.slopeLimit = 90.0f;
+        /* float timeInAir = 0.0f;
+         charController.slopeLimit = 90.0f;
 
-        do
-        {
-            float jumpForce = jumpFallOff.Evaluate(timeInAir);
-            charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
-            timeInAir += Time.deltaTime;
+         do
+         {
+             float jumpForce = jumpFallOff.Evaluate(timeInAir);
+             charController.Move(Vector3.up * jumpForce * jumpMultiplier * Time.deltaTime);
+             timeInAir += Time.deltaTime;
 
-            yield return null;
+             yield return null;
 
-        } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
+         } while (!charController.isGrounded && charController.collisionFlags != CollisionFlags.Above);
 
 
-        charController.slopeLimit = 45.0f;
-        isJumping = false;
-
+         charController.slopeLimit = 45.0f;
+         isJumping = false;
+         */
+        return null;
     }
 
 
