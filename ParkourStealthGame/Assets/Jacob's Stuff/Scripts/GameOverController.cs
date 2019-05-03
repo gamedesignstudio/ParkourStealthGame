163c8c7dc0667a40fb668a25899036d636710ef5
Jacob Greene <jgree178@uncc.edu>
2019-05-03 15:58:54 -0400

    Game Over Assets
    Add new scene and controller

---
+++ ParkourStealthGame/Assets/Jacob's Stuff/Scripts/GameOverController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class GameOverController : MonoBehaviour
+{
+    [SerializeField] private InputField input;
+    [SerializeField] private Scene sceneToLoad;
+
+    [SerializeField] private TextMeshProUGUI scoreText;
+    private int finalScore;
+
+    [SerializeField] private TextMeshProUGUI timeText;
+    private string finalTime;
+
+    private void Awake() {
+        //Setup InputField Restrictions
+        //input = GameObject.Find("InputField").GetComponent<InputField>();
+        input.characterLimit = 3;
+        input.ActivateInputField();
+
+        //Setup Score and Time display
+        finalScore = PlayerPrefs.GetInt("PlayerScore");
+        scoreText.text = finalScore.ToString();
+
+        finalTime = PlayerPrefs.GetString("PlayerTimeString");
+        timeText.text = finalTime;
+    }
+
+    public void GetInput(string name) {
+        name = input.text;
+        name = name.ToUpper();
+        PlayerPrefs.SetString("PlayerName", name);
+        Debug.Log("Saved Name: " + PlayerPrefs.GetString("PlayerName"));
+    }
+
+    public void LoadScene() {
+        Debug.Log("Opening Scene: " + sceneToLoad.name);
+        SceneManager.LoadScene(sceneToLoad.handle);
+        PlayerPrefs.SetInt("AddEntry", 1);
+    }
+}
